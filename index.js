4e50f052291e10fe29381635657e895e3b01ac0d
gp622000 <Gyanprakash3434@gmail.com>
2022-03-02 19:35:29 +0530

    cotact form added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -5,8 +6,36 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+var contactList = [
+  {
+    name: "Gyan",
+    phone: "123123123",
+  },
+  {
+    name: "prakash",
+    phone: "12312312323",
+  },
+  {
+    name: "kumar",
+    phone: "389434394349",
+  },
+];
+
 app.get("/gyan", (req, res) => {
-  res.send("Hello gyan");
+  console.log(res);
+  return res.render("home", {
+    title: "this is gyan",
+  });
+});
+
+app.get("/", (req, res) => {
+  return res.render("contact", {
+    contactList,
+  });
+});
+
+app.post("/contact", (req, res) => {
+  return res.redirect("/");
 });
 
 app.listen(8000, (err) => {
