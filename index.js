852894510bc04057f8fef6bdd0b0d420c82cd9e3
gp622000 <Gyanprakash3434@gmail.com>
2022-03-02 22:08:04 +0530

    static file assets updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded());
-app.use(express.json());
+app.use(express.static("assets"));
 
 var contactList = [
   {
