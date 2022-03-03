0cb3255bf205a4dccfcbdc634e123f5a7fe1a55f
gp622000 <Gyanprakash3434@gmail.com>
2022-03-03 14:42:31 +0530

    add contact delete contact updated

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { json } = require("body-parser");
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const Contact = require("./models/contact");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/contact")
+  .then(() => console.log("Connected to mongodb database sucessfully"))
+  .catch((err) => console.log("Error", err));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -9,41 +16,55 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded());
 app.use(express.static("assets"));
 
-var contactList = [
-  {
-    name: "Gyan",
-    phone: "123123123",
-  },
-  {
-    name: "prakash",
-    phone: "12312312323",
-  },
-  {
-    name: "kumar",
-    phone: "389434394349",
-  },
-];
+// var contactList = [
+//   {
+//     name: "Gyan",
+//     phone: "123123123",
+//   },
+//   {
+//     name: "prakash",
+//     phone: "12312312323",
+//   },
+//   {
+//     name: "kumar",
+//     phone: "389434394349",
+//   },
+// ];
 
 app.get("/", (req, res) => {
-  return res.render("contact", {
-    contactList,
+  Contact.find({}, (err, contacts) => {
+    if (err) {
+      console.log("Error in fetching data from db");
+      return;
+    }
+    return res.render("contact", {
+      contactList: contacts,
+    });
   });
 });
 
-app.post("/add-contact", (req, res) => {
-  console.log(req.body);
-  contactList.push({
+app.post("/add-contact", async (req, res) => {
+  const contact = new Contact({
     name: req.body.name,
     phone: req.body.phone,
   });
+  await contact.save();
+
   return res.redirect("/");
 });
 
 app.get("/delete-contact/:phone", (req, res) => {
-  contactList = contactList.filter((contact) => {
-    return contact.phone !== req.params.phone;
+  Contact.findOneAndDelete({ phone: req.params.phone }, (err, contact) => {
+    if (err) {
+      console.log("Err", err);
+      return;
+    }
+    console.log(contact);
+    return res.redirect("/");
   });
-  return res.redirect("/");
+  //   contactList = contactList.filter((contact) => {
+  //     return contact.phone !== req.params.phone;
+  //   });
 });
 
 app.listen(8000, (err) => {
